ec9baf7a2406bd31d785cb26e671537b79e40e56
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2019-09-12 18:23:03 +0200

    non working kestrel implementation

--- src/EventStore.Core/Services/Transport/Http/IHttpService.cs
+++ src/EventStore.Core/Services/Transport/Http/IHttpService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using EventStore.Core.Bus;
+using EventStore.Core.Messages;
 using EventStore.Transport.Http.EntityManagement;
 
 namespace EventStore.Core.Services.Transport.Http {
@@ -15,9 +17,13 @@ namespace EventStore.Core.Services.Transport.Http {
 		bool ForwardRequest(HttpEntityManager manager);
 	}
 
-	public interface IHttpService {
+	public interface IHttpService :
+		IHandle<SystemMessage.SystemInit>,
+		IHandle<SystemMessage.BecomeShuttingDown>,
+		IHandle<HttpMessage.PurgeTimedOutRequests> {
 		IEnumerable<string> ListenPrefixes { get; }
 		ServiceAccessibility Accessibility { get; }
+		bool IsListening { get; }
 
 		List<UriToActionMatch> GetAllUriMatches(Uri uri);
 		void SetupController(IHttpController controller);
@@ -26,5 +32,7 @@ namespace EventStore.Core.Services.Transport.Http {
 			Func<HttpEntityManager, UriTemplateMatch, RequestParams> handler);
 
 		void RegisterAction(ControllerAction action, Action<HttpEntityManager, UriTemplateMatch> handler);
+
+		void Shutdown();
 	}
 }
